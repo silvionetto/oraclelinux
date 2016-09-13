3b268e5f8473e12227f9ec0e1e62e52a3570c179
Sílvio Netto <silvio.netto@gmail.com>
2016-09-13 17:08:40 +0200

    Update setup.sh

--- oraclelinux/setup.sh
+++ oraclelinux/setup.sh
@@ -17,25 +17,25 @@ yum install -y sysstat &&
 
 # ODBC Driver
 yum install -y unixODBC &&
-yum install -y unixODBC-devel&&
+yum install -y unixODBC-devel &&
 
-yum install -y tar
-yum install -y less
-yum install -y sudo
+yum install -y tar &&
+yum install -y less &&
+yum install -y sudo &&
 
-source /root/.bash_profile
+source /root/.bash_profile &&
 
-groupadd oinstall
-groupadd dba
-useradd -g oinstall oracle
-usermod -a -G dba oracle
-mkdir -p /oracledb/app/
-chown -R oracle:oinstall /oracledb/app/
-chmod -R 775 /oracledb/app/
+groupadd oinstall &&
+groupadd dba &&
+useradd -g oinstall oracle &&
+usermod -a -G dba oracle &&
+mkdir -p /oracledb/app/ &&
+chown -R oracle:oinstall /oracledb/app/ &&
+chmod -R 775 /oracledb/app/ &&
 
-mkdir -p /oracledb/tmp/
-chmod a+wr /oracledb/tmp
+mkdir -p /oracledb/tmp/ &&
+chmod a+wr /oracledb/tmp &&
 
-unzip V46095-01_1of2.zip
-unzip V46095-01_2of2.zip 
+unzip V46095-01_1of2.zip &&
+unzip V46095-01_2of2.zip  &&
 rm V46095-01_1of2.zip V46095-01_2of2.zip

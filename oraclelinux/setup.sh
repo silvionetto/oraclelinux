dfcb11ba83bf3964d1a1bdeeaa60952ca5f7a7e5
Silvio Netto <silvio.netto@gmail.com>
2016-09-13 22:13:51 +0200

    Adding variables

--- oraclelinux/setup.sh
+++ oraclelinux/setup.sh
@@ -1,64 +1,29 @@
 #!/bin/bash
 
-yum update -y &&
+mkdir -p /usr/local/java/
+tar xvzf oraclelinux/jdk-8u102-linux-x64.tar.gz -C /usr/local/java/ &&
+rm oraclelinux/jdk-8u102-linux-x64.tar.gz &&
 
-# System Requirements
-yum install -y compat-libcap1 &&
-yum install -y gcc &&
-yum install -y gcc-c++ &&
-yum install -y glibc &&
-yum install -y glibc-devel &&
-yum install -y libgcc &&
-yum install -y libstdc++ &&
-yum install -y libstdc++ &&
-yum install -y libaio &&
-yum install -y libaio-devel &&
-yum install -y sysstat &&
+echo “JAVA_HOME=/usr/local/java/jdk1.8.0_102” >> /root/.bash_profile
+echo “export JAVA_HOME” >> /root/.bash_profile
+echo “PATH=$PATH:$JAVA_HOME/bin” >> /root/.bash_profile
 
-# ODBC Driver
-yum install -y unixODBC &&
-yum install -y unixODBC-devel&&
+source /root/.bash_profile &&
 
-# Tools
-yum install -y tar &&
-yum install -y less &&
-yum install -y sudo &&
-yum install -y unzip
+groupadd oinstall &&
+groupadd dba &&
+useradd -g oinstall oracle &&
+usermod -a -G dba oracle &&
+mkdir -p /oracledb/app/ &&
+chown -R oracle:oinstall /oracledb/app/ &&
+chmod -R 775 /oracledb/app/ &&
 
-# System Requirements
-yum install -y compat-libcap1
-yum install -y gcc
-yum install -y gcc-c++
-yum install -y glibc
-yum install -y glibc-devel
-yum install -y libgcc
-yum install -y libstdc++
-yum install -y libstdc++
-yum install -y libaio
-yum install -y libaio-devel
-yum install -y sysstat
+mkdir -p /oracledb/tmp/ &&
+chmod a+wr /oracledb/tmp &&
 
-# ODBC Driver
-yum install -y unixODBC
-yum install -y unixODBC-devel
+unzip oraclelinux/V46095-01_1of2.zip &&
+unzip oraclelinux/V46095-01_2of2.zip &&
+rm oraclelinux/V46095-01_1of2.zip &&
+rm oraclelinux/V46095-01_2of2.zip
 
-yum install -y tar
-yum install -y less
-yum install -y sudo
-
-source /root/.bash_profile
-
-groupadd oinstall
-groupadd dba
-useradd -g oinstall oracle
-usermod -a -G dba oracle
-mkdir -p /oracledb/app/
-chown -R oracle:oinstall /oracledb/app/
-chmod -R 775 /oracledb/app/
-
-mkdir -p /oracledb/tmp/
-chmod a+wr /oracledb/tmp
-
-unzip V46095-01_1of2.zip
-unzip V46095-01_2of2.zip 
-rm V46095-01_1of2.zip V46095-01_2of2.zip
+exit $?
